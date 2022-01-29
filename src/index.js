3eb8fb2efb00dd5b95ac151503d5115e9d42907d
JonathanB <jbaquirin@gmail.com>
2022-01-29 16:04:36 +0200

    finalizing basic data fetch

--- src/index.js
+++ src/index.js
@@ -6,12 +6,44 @@ import reportWebVitals from './reportWebVitals';
 
 //Do tasks here
 //2.Use fetch-api to establish connection to JSON
-fetch('https://gist.githubusercontent.com/VincentLeV/a0c326b9cbeabf63b4e5e02aa9779f6c/raw/b916a9e3d40aef926bf7e3b9b4db308d7da1ca5d/shares.json')
-  .then(response => response.json())
-  .then(data => console.log(data))
+const fetchCOmpanies = () => {
+  const companies = [];
+  fetch('https://gist.githubusercontent.com/VincentLeV/a0c326b9cbeabf63b4e5e02aa9779f6c/raw/b916a9e3d40aef926bf7e3b9b4db308d7da1ca5d/shares.json')
+    .then(response => response.json())
+    .then(data => companies.push(data))
+    .catch(error => console.error(error))
+  return companies;
+}
+
 //3.yeild computation
+const companies = fetchCOmpanies();
+console.log(companies);
+
 //4.average dividend computation (last 5 years)
+
 //5. Weighted average computation
+
+//**Output data format: array of company objects**
+/*   const outputData = [
+    {
+      Share:
+      Company: Company1,
+      Price:
+      Last year dividend: 0.75
+      Dividend yield-%: 2.3
+      Five-year average dividend yield-%: X.Y
+      Five-year weighted average dividend yield-%: W.Z
+    },
+    {
+      Share:
+      Company: Company2,
+      Price:
+      Last year dividend: 0.75
+      Dividend yield-%: 2.3
+      Five-year average dividend yield-%: X.Y
+      Five-year weighted average dividend yield-%: W.Z
+    },
+  ] */
 
 
 
