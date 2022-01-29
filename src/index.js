eac06d1e8451ce2950490e0a17defc989337ad44
JonathanB <jbaquirin@gmail.com>
2022-01-29 15:26:41 +0200

    basic data fetch done

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 //Do tasks here
-  //2.Use fetch-api to establish connection to JSON
-  //3.yeild computation
-  //4.average dividend computation (last 5 years)
-  //5. Weighted average computation
+//2.Use fetch-api to establish connection to JSON
+fetch('https://gist.githubusercontent.com/VincentLeV/a0c326b9cbeabf63b4e5e02aa9779f6c/raw/b916a9e3d40aef926bf7e3b9b4db308d7da1ca5d/shares.json')
+  .then(response => response.json())
+  .then(data => console.log(data))
+//3.yeild computation
+//4.average dividend computation (last 5 years)
+//5. Weighted average computation
 
 
 
