62dd9cd0c8b13dfaf1b8d1ce0cc4d8f6d9ad1872
Giovani Francisco <giovani.francisco@myy.haaga-helia.fi>
2022-01-30 14:13:12 +0200

    updates number 4

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 //2.Use fetch-api to fetch JSON
-const companiesUrl = 'https://gist.githubusercontent.com/VincentLeV/a0c326b9cbeabf63b4e5e02aa9779f6c/raw/b916a9e3d40aef926bf7e3b9b4db308d7da1ca5d/shares.json'
+const companiesUrl =
+  "https://gist.githubusercontent.com/VincentLeV/a0c326b9cbeabf63b4e5e02aa9779f6c/raw/b916a9e3d40aef926bf7e3b9b4db308d7da1ca5d/shares.json";
 
 const fetchCOmpanies = async (url) => {
   const response = await fetch(url);
@@ -13,36 +14,67 @@ const fetchCOmpanies = async (url) => {
     const data = await response.json();
     return data;
   } else {
-    throw new Error('Bad response')
+    throw new Error("Bad response");
   }
-}
+};
 
 //MAIN App
 const dividendApp = async () => {
   //3.yeild computation function
   const getDividendYield = (company) => {
     const dividendHistory = company.dividendHistory;
-    const yearDividend = dividendHistory.filter(dividend => dividend.year === 2021);
+    const yearDividend = dividendHistory.filter(
+      (dividend) => dividend.year === 2021
+    );
     if (yearDividend[0]) {
       const dividendYield = (yearDividend[0].dividend / company.price) * 100;
       return Math.round(dividendYield * 100) / 100;
     } else {
-      throw new Error('Dividend for year does not exist')
+      throw new Error("Dividend for year does not exist");
     }
+  };
 
-  }
+  //4.average dividend computation (last 5 years) function
+  const companies = await fetchCOmpanies(companiesUrl);
+  const getCompanyDividends = () => {
+    const getDividendArray = [
+      companies.map((company) => company.dividendHistory),
+    ];
 
-  //4.average dividend computation (last 5 years) function 
+    const getPrice = [companies.map((company) => company.price)];
+
+    let sumArray = [];
+
+    getDividendArray.map(function (dividendHistory) {
+      dividendHistory.map(function (dividends) {
+        let initialValue = 0;
+        let sum = dividends
+          .reduce(function (previousValue, currentValue) {
+            return previousValue + currentValue.dividend;
+          }, initialValue)
+          .toFixed(2);
+        sumArray.push(parseFloat(sum));
+      });
+    });
+
+    const prices = getPrice[0];
+    const result = sumArray.map(function (n, i) {
+      return parseFloat((n / 5 / prices[i]).toFixed(2));
+    });
+    console.log("RESULT of 5 Year Average Dividend Yield: ", result);
+  };
+
+  getCompanyDividends();
 
   //5. Weighted average computation function
 
   try {
     const companies = await fetchCOmpanies(companiesUrl);
-    console.log(companies)
+    console.log(companies);
 
     //**Output data format: array of company objects**
     const getShares = () => {
-      return companies.map(company => ({
+      return companies.map((company) => ({
         share: company.share,
         company: company.company,
         price: company.price,
@@ -50,60 +82,27 @@ const dividendApp = async () => {
         dividendYield: getDividendYield(company),
         //AveYield: getAveYield(company),
         //weightedAve: getWeightedAve(company)
-      })
-      )
-    }
+      }));
+    };
 
     const initialSharesArray = getShares(companies);
     console.log(initialSharesArray);
 
     //Final output formatting
-
   } catch (error) {
     if (error instanceof Error) {
       console.log(`Something went wrong, Error: ${error.message}`);
     }
   }
-}
+};
 
 dividendApp();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
